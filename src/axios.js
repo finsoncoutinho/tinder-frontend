d807272a0177b2a18664bc8d79f987c613800a3e
FINSON COUTINHO <69283994+finsoncoutinho@users.noreply.github.com>
2023-03-20 22:20:43 +0530

    Updated API endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://tinder-backend-server1.herokuapp.com",
+  baseURL: "https://tinder-backend-9tvs.onrender.com/",
 });
 
 export default instance;
